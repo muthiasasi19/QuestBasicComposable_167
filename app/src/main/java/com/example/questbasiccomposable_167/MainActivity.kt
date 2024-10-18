73e57958723d3f1365e97c1b3ea77ab995f52896
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-10-18 15:07:00 +0700

    Mengatur ukuran dan sudut atau bentuk gambar

--- app/src/main/java/com/example/questbasiccomposable_167/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_167/MainActivity.kt
@@ -10,11 +10,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -57,8 +60,12 @@ fun BasicColumn(modifier: Modifier = Modifier){
                 Image(
                     painter = painterResource(id = R.drawable.umy),
                     contentDescription = "", // harus ada
+                    modifier = Modifier
+                        .size(200.dp)
+                        .clip(CircleShape))
 
-                    )
+
+                )
 
             }
             }
