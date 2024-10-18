a1ca666a42e0e05664d6d6a35a81113f645236d1
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-10-18 15:04:19 +0700

    Mengambil gambar dari sumber drawable dengan ID umy.

--- app/src/main/java/com/example/questbasiccomposable_167/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_167/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -14,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -52,6 +54,10 @@ fun BasicColumn(modifier: Modifier = Modifier){
                     fontStyle = FontStyle.Normal)
 
                 Spacer(modifier = Modifier.padding(5.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.umy),
+                )
+
             }
             }
 @Composable
