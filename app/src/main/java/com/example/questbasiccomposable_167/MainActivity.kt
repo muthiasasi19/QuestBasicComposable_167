96e989388ca431942d50bb77a6c72238c0adf9c8
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-10-18 14:20:05 +0700

    Commit

---
+++ app/src/main/java/com/example/questbasiccomposable_167/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.questbasiccomposable_167
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questbasiccomposable_167.ui.theme.QuestBasicComposable_167Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestBasicComposable_167Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {}
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuestBasicComposable_167Theme {
+        Greeting("Android")
+    }
+}
