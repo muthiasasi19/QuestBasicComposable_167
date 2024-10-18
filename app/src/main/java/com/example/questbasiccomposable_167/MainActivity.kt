f5a593e913902266300096a2469d8510c288bb88
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-10-18 14:30:14 +0700

    Memanggil fungsi BasicColumn, memberikan padding di sekeliling halaman/konten, berdasarkan innerpadding dari Scaffold

--- app/src/main/java/com/example/questbasiccomposable_167/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_167/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -19,7 +20,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuestBasicComposable_167Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    BasicColumn(modifier = Modifier.padding(innerPadding))
+
                 }
             }
         }
